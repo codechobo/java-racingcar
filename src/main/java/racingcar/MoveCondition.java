92c57761487b50ccdbcc13a90de759975e8e5af1
이경범 <tkdwk567@naver.com>
2022-12-10 17:37:36 +0900

    [feat] 이동조건 기능 생성

---
+++ src/main/java/racingcar/MoveCondition.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public class MoveCondition implements Condition {
+
+    private RandomNumberGenerator randomNumberGenerator;
+
+    public MoveCondition(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    @Override
+    public boolean isCondition() {
+        int randomNumber = randomNumberGenerator.generate();
+
+        if (randomNumber >= 4) {
+            return true;
+        }
+
+        return false;
+    }
+}
