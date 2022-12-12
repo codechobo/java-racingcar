296ed563c3058de328d147ee6606b56363055da1
이경범 <tkdwk567@naver.com>
2022-12-12 14:24:37 +0900

    [refactor] 게임 시작 refactor

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public class Game {
 
+    private Referee referee;
     private List<Car> cars = new ArrayList<>();
+
+    public Game(Referee referee) {
+        this.referee = referee;
+    }
 
     public void start(List<String> carNames, int step) {
         this.cars.addAll(Car.createCars(carNames));
@@ -24,6 +29,8 @@ public class Game {
                 break;
             }
         }
+
+        OutputView.printVictoryCar(referee.findVictoryCarName(this.cars));
     }
 
     private boolean engineStatusByMove(List<Car> cars, int step, boolean isOff) {
@@ -33,5 +40,9 @@ public class Game {
         }
 
         return isOff;
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
     }
 }
